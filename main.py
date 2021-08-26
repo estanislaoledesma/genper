a2755075d852352951f55d4ce8d5043e72525bdd
Estanislao Ledesma <estanislaomledesma@gmail.com>
2021-08-25 21:20:13 -0300

    Arreglado el script de generación de imagenes

--- main.py
+++
@@ -1,5 +0,0 @@
-from dataloader.image_generator import ImageGenerator
-
-if __name__ == "__main__":
-    image_generator = ImageGenerator()
-    image_generator.generate_images()
--- scripts/generate_images.py
+++ scripts/generate_images.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from dataloader.image_generator import ImageGenerator
 
 if __name__ == "__main__":
